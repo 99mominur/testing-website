7e790bf910b35bcf76ddea2f4777809c29d9b328
Mominur Rahman <67354193+99mominur@users.noreply.github.com>
2020-06-24 11:12:45 +0600

    Add files via upload

---
+++ c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+float main()
+{
+
+ float a,b,c;
+clrscr();
+printf(" Enter First Number:")   ;
+     scanf %f("%d",&a);
+       printf(" Enter secound Number:")        ;
+     scanf %f("%d",&b)       ;
+
+    c=a+b;
+
+     printf("%d+%d=%d\n",a,b,c);
+
+    getch();
+     return0;
+}
